77fdc555a65f240444bddd1485ffba690a3e3096
Martin M <Martin M@192.168.0.32>
2017-05-26 10:50:19 +0200

    second step: register und login gebaut, aber nicht verkabelt und getestet

--- sites/produkte.php
+++ sites/produkte.php
@@ -1,4 +1,12 @@
 <?php
+        $db = new DB();
+        $db->doConnect();
+        $ProdukteKonkret = new ProduktListe(); ### Ich blick nicht mehr durch ob ich das brauch       
+        $ProdukteKonkret = $db->getProduktListe();
+        
+        foreach ($ProdukteKonkret ->PL as $Produkt){
+            
+        }
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
