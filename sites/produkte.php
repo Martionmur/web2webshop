4056c61df00de64dd8c1da220c727b2db9bbea36
Martin M <Martin M@192.168.0.32>
2017-05-26 22:40:16 +0200

    second step: register und login gebaut, aber nicht verkabelt und getestet

--- sites/produkte.php
+++ sites/produkte.php
@@ -2,10 +2,11 @@
         $db = new DB();
         $db->doConnect();
         $ProdukteKonkret = new ProduktListe(); ### Ich blick nicht mehr durch ob ich das brauch       
-        $ProdukteKonkret = $db->getProduktListe();
-        
-        foreach ($ProdukteKonkret ->PL as $Produkt){ 
-        }
+        #$ProdukteKonkret = $db->getProduktListe();
+        $ProdukteKonkret.
+        produktListenAnzeige($Produktliste)
+            
+            
         
   ?>      
 <!-- DRAGGABLE IN HEAD -->
@@ -19,31 +20,33 @@
 
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  
   <script>
   $( function() {
-    $( "#draggable" ).draggable();
+    $( ".ProdTile" ).draggable(); 
   } );
   </script>
+ <!-- right kind of dragable?-->
 </head>
 <body>
+    
+   <?php
+        $db = new DB();
+        $db->doConnect();
+        $ProdukteKonkret = new ProduktListe(); ### Ich blick nicht mehr durch ob ich das brauch       
+        #$ProdukteKonkret = $db->getProduktListe();
+        $ProdukteKonkret.fillProduktliste();
+        produktListenAnzeige($ProdukteKonkret.fillProduktliste())
+            
+            
+        
+  ?>  
+    
+</body>
+    
+    
  
-<div id="draggable" class="ui-widget-content">
-  <p>Drag me around</p>
-</div>
 
 
 
-## produktliste mit caption carten
-<div class="row">
-  <div class="ProdTile" id="ProdTile.Pid"> #draggable
-    <div class="thumbnail">
-      <img src="res/img/prod1".pid alt=Bezeichnung>
-      <div class="caption">
-        <h4>Bezeichnung</h4>
-        <p>Preis." €"</p>
-        <p>Bewertung."/10 Bewertung"</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
-    </div>
-  </div>
-</div>
+
